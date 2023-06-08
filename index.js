41ed4ab463da24174a71f97a0b654a0eb9de381e
Arman7052 <99arman7052@gmail.com>
2023-06-08 17:50:09 +0600

    Hello Summer

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/',(req,res) => {
+    res.send('Summer is coming...');
+})
+app.listen(port, ()=> {
+    console.log(`summer is showing on port ${port}`);
+})
+
+
+
