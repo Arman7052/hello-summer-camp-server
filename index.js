d621302cea3e56a0093dea44c49a29c793f5e020
Arman7052 <99arman7052@gmail.com>
2023-06-13 23:36:41 +0600

    verifyJWT

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ app.use(cors());
 app.use(express.json());
 
 
+const verifyJWT= (req,res,next) =>{
+    const authorization =req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: 'Unauthorize Access'} );
+    }
+
+    const token = authorization.split('')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(401).send({error: true, message: 'Unauthorize Access'});
+        }
+        req.decoded =decoded;
+        next();
+    })
+}
+
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pf5eojy.mongodb.net/?retryWrites=true&w=majority`;
@@ -60,6 +77,16 @@ async function run() {
         })
 
         //-------------------POST-------------------------//
+
+        // JWT 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({ token })
+        })
+
+
         // User Database 
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -83,7 +110,7 @@ async function run() {
         })
 
         // ------------------PATCH--------------------------//
-        
+
         //make Admin// 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
